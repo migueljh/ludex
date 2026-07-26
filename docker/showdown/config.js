bc99e4cae61ce6dfdd3395b9162f07a6f88634bf
Miguel <miguelhernandezjp@gmail.com>
2026-07-26 12:27:54 -0300

    feat(infra): server local de Showdown bajo profile local

---
+++ docker/showdown/config.js
@@ -0,0 +1,8 @@
+'use strict';
+exports.port = 8000;
+exports.bindaddress = '0.0.0.0';
+exports.workers = 1;
+exports.nothrottle = true;
+exports.noguestsecurity = true;
+exports.backdoor = false;
+exports.report_joins = false;
